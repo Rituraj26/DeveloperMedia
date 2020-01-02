457f1a9fda6c04b86e61b63fe18890f016168319
Rituraj <riturajguwahati26@gmail.com>
2020-01-02 14:06:13 +0530

    fixed login and register issues

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,10 @@
 import React, { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
+import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import { login } from "../../actions/auth";
 
-const Login = ({ login }) => {
+const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     email: "",
     password: ""
@@ -12,21 +12,23 @@ const Login = ({ login }) => {
 
   const { email, password } = formData;
 
-  const onChange = e => {
+  const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    login({ email, password });
   };
 
-  const onSubmit = e => {
-    e.preventDefault();
-    login(email, password);
-  };
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
 
   return (
     <Fragment>
-      <div className="alert alert-danger">Invalid credentials</div>
       <h1 className="large text-primary">Sign In</h1>
       <p className="lead">
-        <i className="fas fa-user"></i> Sign into Your Account
+        <i className="fas fa-user" /> Sign Into Your Account
       </p>
       <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
@@ -46,6 +48,7 @@ const Login = ({ login }) => {
             name="password"
             value={password}
             onChange={e => onChange(e)}
+            minLength="6"
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Login" />
@@ -58,7 +61,12 @@ const Login = ({ login }) => {
 };
 
 Login.propTypes = {
-  login: PropTypes.func.isRequired
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
 
-export default connect(null, { login })(Login);
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);
