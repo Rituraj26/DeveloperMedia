2dd41890187e9488361e1edaaff89f0b29e06f15
Rituraj <riturajguwahati26@gmail.com>
2020-01-02 20:34:56 +0530

    added clear profile functionality

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,7 +5,8 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE
 } from "../actions/types";
 
 const initialState = {
@@ -45,6 +46,14 @@ export default function(state = initialState, action) {
         isAuthenticated: true,
         loading: false,
         user: payload
+      };
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
+        loading: false,
+        user: null
       };
     default:
       return state;
